c194c894dbf1eca0eda6f75b0306c79d986f3c10
Madhav Lodha <madhavlodha2503@gmail.com>
2025-08-14 19:20:33 +0000

    fix: resolve Select dropdown diagonal sliding issue
    Set `transform-origin: top` to prevent diagonal spawn.
    
    #VERCEL_SKIP

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -69,6 +69,7 @@ const SelectContent = React.forwardRef<
       ref={ref}
       className={cn(
         "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md",
+        "[transform-origin:top] data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
         className,
       )}
       position={position}
